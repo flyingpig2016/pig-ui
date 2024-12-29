b35e4e94c76a85567872bdcc139d75ed6e3676b2
薛棒棒 <xuebb3@asiainfo.com>
2024-12-29 23:14:58 +0800

    gitgnore

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+    }
+  },
+   // 强制内联CSS（使用组件时，不需要再引入css）
+  css: {
+    extract: false
+  },
+
+
 })
